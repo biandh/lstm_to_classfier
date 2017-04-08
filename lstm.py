e67908634bdcb83db44718e1800e12ea208a6e9d
biandh <bian2015@mail.ustc.edu.cn>
2017-04-08 10:35:24 +0800

    Update lstm.py

--- lstm.py
+++ lstm.py
@@ -8,8 +8,6 @@ from tensorflow.contrib import rnn
 import helper
 import os
 import numpy as np
-# Import MNIST data
-from tensorflow.examples.tutorials.mnist import input_data
 
 # Parameters
 learning_rate = 0.001
@@ -17,10 +15,10 @@ training_iters = 10000
 batch_size = 128
 display_step = 50
 # Network Parameters
-n_input = 100 # MNIST data input (img shape: 28*28)
+n_input = 100 #  data input (100 dim)
 n_steps = 30 # timesteps
 n_hidden = 128 # hidden layer num of features
-n_classes = 2 # MNIST total classes (0-9 digits)
+n_classes = 2 # total classes (0-1)
 
 # tf Graph input
 x = tf.placeholder(tf.int32, [None, n_steps, n_input])
@@ -35,6 +33,7 @@ weights = {
 biases = {
     'out': tf.Variable(tf.random_normal([n_classes]))
 }
+
 train_path = 'train.2'
 char2id_file = 'char2id'
 label2id_file = 'label2id'
@@ -80,8 +79,6 @@ def last_relevant(output, length):
     return relevant
 
 def RNN( x,weights, biases):
-
-    # Prepare data shape to match `rnn` function requirements
     # Current data input shape: (batch_size, n_steps, n_input)
     # Required shape: 'n_steps' tensors list of shape (batch_size, n_input)
     length = tf.reduce_sum(tf.sign(x1), reduction_indices=1)
@@ -89,16 +86,16 @@ def RNN( x,weights, biases):
     # Permuting batch_size and n_steps
     x = tf.transpose(x, [1, 0, 2])
 
-    # # Reshaping to (n_steps*batch_size, n_input)
+    # Reshaping to (n_steps*batch_size, n_input)
     x = tf.reshape(x, [-1, n_input])
 
-    # # Split to get a list of 'n_steps' tensors of shape (batch_size, n_input)
+    # Split to get a list of 'n_steps' tensors of shape (batch_size, n_input)
     x = tf.split(x, n_steps, 0)
     # Define a lstm cell with tensorflow
     lstm_cell = rnn.BasicLSTMCell(n_hidden, forget_bias=1.0)
     # Get lstm cell output
     outputs, states = rnn.static_rnn(lstm_cell, x, dtype=tf.float32 )
-    # outputs = last_relevant(outputs, length)
+    outputs = last_relevant(outputs, length)
     # Linear activation, using rnn inner loop last output
     return tf.matmul( outputs[-1], weights['out'] ) + biases['out'], outputs
 
